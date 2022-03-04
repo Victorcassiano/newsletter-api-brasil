529b24dde0a625e68d9b58bbb8d29bce9ff42485
Victor Cassiano <43105344+Victorcassiano@users.noreply.github.com>
2022-03-04 18:24:21 -0300

    Primeiro commit

---
+++ src/server.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const puppeteer = require('puppeteer')
+var cors = require('cors')
+const app = express();
+
+app.use(express.json())
+app.use(cors())
+
+
+app.listen(3000, () => {
+    const date = new Date().toLocaleString("pt-BR", { timeZone: 'America/Sao_Paulo' })
+
+    console.log(`Servidor online 🚀 ${date}`)
+})
+
+app.post('/news/:pages', async (response, request) => {
+    const { pages } = response.params
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(`https://www.tecmundo.com.br/api/v1/materia/novidades/?page=${pages}&top=25`);
+
+    const json = await page.evaluate(() => {
+        const json = document.querySelector('body pre').innerHTML
+
+        return JSON.parse(json);
+    })
+
+
+    await browser.close();
+
+    return request.json(json.dados)
+})
+
+app.post('/curiosity/:amount', async (response, request) => {
+    const { amount } = response.params
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(`https://www.tecmundo.com.br/api/v1/externo/megacurioso/semana/${amount}`);
+
+    const json = await page.evaluate(() => {
+        const json = document.querySelector('body pre').innerHTML
+
+        return JSON.parse(json);
+    })
+
+
+    await browser.close();
+
+    return request.json(json.dados)
+})
+
